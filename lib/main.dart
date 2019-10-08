fc0704c62a89ad7f403e90c0dd53fcc365f32499
Anurag Bhandari <anurag.bhd@gmail.com>
2019-10-08 13:04:38 +0530

    Add a custom font 'Poppins' and set it as default

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class SSMenuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'Poppins'),
       home: MenuScreen(),
     );
   }

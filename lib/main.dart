638ecb30ba11165415849f0498015c4ef6561093
Anurag Bhandari <anurag.bhd@gmail.com>
2019-10-08 16:37:09 +0530

    Initialize global app state in the root widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:ss_menu/screens/menu_screen.dart';
+import 'package:ss_menu/models/app_state.dart';
 
 void main() => runApp(SSMenuApp());
 
-class SSMenuApp extends StatelessWidget {
+class SSMenuApp extends StatefulWidget {
+  @override
+  _SSMenuAppState createState() => _SSMenuAppState();
+}
+
+class _SSMenuAppState extends State<SSMenuApp> {
+  AppState appState = AppState();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(fontFamily: 'Poppins'),
-      home: MenuScreen(),
+    return ChangeNotifierProvider(
+      builder: (context) => this.appState,
+      child: MaterialApp(
+        theme: ThemeData(fontFamily: 'Poppins'),
+        home: MenuScreen(),
+      ),
     );
   }
 }

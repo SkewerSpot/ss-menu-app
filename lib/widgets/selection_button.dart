8d20ea8f56c97a5e13712d71d28504862bd8942f
Anurag Bhandari <anurag.bhd@gmail.com>
2019-10-13 08:57:24 +0530

    Set default color of SelectionButton to yellow

--- lib/widgets/selection_button.dart
+++ lib/widgets/selection_button.dart
@@ -26,7 +26,7 @@ class SelectionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RaisedButton(
-      color: this.isSelected ? kThemeColorRed : Colors.transparent,
+      color: this.isSelected ? kThemeColorRed : kThemeColorYellow,
       child: Text(
         this.value,
         style: TextStyle(

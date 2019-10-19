4a60e9743b79af3ca705daab6b8a9499e9044a80
Anurag Bhandari <anurag.bhd@gmail.com>
2019-10-19 18:06:43 +0530

    Add util method to generate new UUID

--- lib/util.dart
+++ lib/util.dart
@@ -1,7 +1,12 @@
 import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
 
 class Util {
   static String getYYYYMMDDDate(DateTime date) {
     return DateFormat('yyyy/MM/dd').format(date);
+  }
+
+  static String newUUID() {
+    return Uuid().v1();
   }
 }
